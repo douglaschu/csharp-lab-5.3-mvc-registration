dc8a078ef03aa4e38026b58f731debdc070c8810
Doug Chu <douglas.l.chu@gmail.com>
2023-08-10 17:02:58 -0400

    added on EF core database lab (5.4) onto coffeeshop website. WHEW

---
+++ mvc-coffeeshop-registration/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using mvc_coffeeshop_registration.Models;
+
+namespace mvc_coffeeshop_registration.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        private CoffeeshopDbContext _coffeeshopDbContext = new CoffeeshopDbContext();
+
+        public IActionResult Index()
+        {
+            List<Product> result = _coffeeshopDbContext.Products.ToList();
+            return View(result);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            Product p = _coffeeshopDbContext.Products.FirstOrDefault(x => x.Id == id );
+            
+            return View(p);
+        }
+    }
+}
---
+++ mvc-coffeeshop-registration/Models/Detail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace mvc_coffeeshop_registration.Models
+{
+   public class Detail
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public decimal? Price { get; set; }
+
+        public string? Category { get; set; }
+    }
+}
+
